0d084db0758974edec5dd3b07163de8b4cdafde0
Kernel Panic <davil544@gmail.com>
2024-03-03 18:00:14 -0500

    Added Credit Card class to allow for payments to be made for tickets

---
+++ DataModels/CreditCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CPIS_Senior_Project.DataModels
+{
+    public class CreditCard
+    {
+        public int CreditCardNumber { get; set; }
+        public string ExpirationDate { get; set; }    //TODO:  Convert Expiration date to DateTime before DB connection is complete
+        public int CVV { get; set; }
+    }
+}
