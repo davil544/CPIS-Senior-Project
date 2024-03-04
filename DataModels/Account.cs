bee28fb31fb26d0b7628d18d5a05f578f6807372
Kernel Panic <davil544@gmail.com>
2024-03-03 19:03:35 -0500

    Created new Account class to replace Credentials.cs (Also added a TODO reminder in the CreditCard class)

---
+++ DataModels/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace CPIS_Senior_Project.DataModels
+{
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string FullName { get; set; }
+        public CreditCard CC { get; set; }
+
+    }
+}
--- DataModels/CreditCard.cs
+++ DataModels/CreditCard.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CPIS_Senior_Project.DataModels
+﻿namespace CPIS_Senior_Project.DataModels
 {
     public class CreditCard
     {
-        public int CreditCardNumber { get; set; }
+        public int CardNumber { get; set; }
         public string ExpirationDate { get; set; }    //TODO:  Convert Expiration date to DateTime before DB connection is complete
         public int CVV { get; set; }
     }
