3ed4f0ce5aa9238d54d818b2609e027f2a4b73f0
Kernel Panic <davil544@gmail.com>
2024-03-04 22:52:49 -0500

    Created Movie Theater class

---
+++ DataModels/Theater.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CPIS_Senior_Project.DataModels
+{
+    public class Theater
+    {
+        public string name {  get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postalCode { get; set; }
+        public string country { get; set; }
+        public string hours { get; set; }
+    }
+}
