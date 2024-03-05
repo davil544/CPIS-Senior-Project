2d1373c21caa3d7cbf1c986f6d12fb772935fd5d
Kernel Panic <davil544@gmail.com>
2024-03-05 17:46:54 -0500

    Added placeholders for movie management (Will become dynamic once Data Access Layer is created and linked with SQL server)

--- Management/Default.aspx
+++ Management/Default.aspx
@@ -17,11 +17,37 @@
                             <h3><asp:Label ID="lblMovieTitle" runat="server" Text="">Title</asp:Label></h3>
                             <p><asp:Label ID="lblMovieSummary" runat="server" Text="">Summary</asp:Label></p>
                             <p><asp:Label ID="lblMoviePrice" runat="server" Text="">$Price.99</asp:Label></p>
-                            <p><asp:Button ID="btnAddMovie" runat="server" BackColor="#337AB7" ForeColor="White" Height="30px" Text="Add to Cart" />
+                            <p><asp:Button ID="btnAddMovie" runat="server" BackColor="#337AB7" ForeColor="White" Height="30px" Text="Modify" /> <!-- Will say Add to Cart on home page -->
                             &nbsp;<asp:Button ID="btnDetailsMovie" runat="server" Height="30px" Text="Details"  /></p>
                         </div>
                    </div>
                 </div>
+               <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <!-- <img src="/Assets/Inventory/poster.jpg" alt="Movie Poster"> -->
+                        <asp:Image ID="Image1" runat="server" alt="Movie Poster Goes Here"/>
+                        <div class="caption">
+                            <h3><asp:Label ID="Label1" runat="server" Text="">Title</asp:Label></h3>
+                            <p><asp:Label ID="Label2" runat="server" Text="">Summary</asp:Label></p>
+                            <p><asp:Label ID="Label3" runat="server" Text="">$Price.99</asp:Label></p>
+                            <p><asp:Button ID="Button1" runat="server" BackColor="#337AB7" ForeColor="White" Height="30px" Text="Modify" /> <!-- Will say Add to Cart on home page -->
+                            &nbsp;<asp:Button ID="Button2" runat="server" Height="30px" Text="Details"  /></p>
+                        </div>
+                   </div>
+               </div>
+               <div class="col-sm-6 col-md-4">
+                   <div class="thumbnail">
+                        <!-- <img src="/Assets/Inventory/poster.jpg" alt="Movie Poster"> -->
+                        <asp:Image ID="Image2" runat="server" alt="Movie Poster Goes Here"/>
+                        <div class="caption">
+                            <h3><asp:Label ID="Label4" runat="server" Text="">Title</asp:Label></h3>
+                            <p><asp:Label ID="Label5" runat="server" Text="">Summary</asp:Label></p>
+                            <p><asp:Label ID="Label6" runat="server" Text="">$Price.99</asp:Label></p>
+                            <p><asp:Button ID="Button3" runat="server" BackColor="#337AB7" ForeColor="White" Height="30px" Text="Modify" /> <!-- Will say Add to Cart on home page -->
+                            &nbsp;<asp:Button ID="Button4" runat="server" Height="30px" Text="Details"  /></p>
+                        </div>
+                   </div>
+               </div>
            </div>
        </div>
    </main>
--- Management/Default.aspx.designer.cs
+++ Management/Default.aspx.designer.cs
@@ -76,5 +76,113 @@ namespace CPIS_Senior_Project.Management
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnDetailsMovie;
+
+        /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// Image2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Label5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Label6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Button3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button3;
+
+        /// <summary>
+        /// Button4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
     }
 }
