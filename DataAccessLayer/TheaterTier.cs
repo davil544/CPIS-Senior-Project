59524c8762626e547084390b4e0dff14320d8521
Kernel Panic <davil544@gmail.com>
2024-03-10 23:33:04 -0400

    Removed unnecessary using directive (Not sure how it got there, pulled it out to reduce server memory usage)

--- DataAccessLayer/TheaterTier.cs
+++ DataAccessLayer/TheaterTier.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.EnterpriseServices.Internal;
 
 namespace CPIS_Senior_Project.DataAccessLayer
 {
