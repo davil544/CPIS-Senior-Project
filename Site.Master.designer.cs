6f70c9f11b1671ecce59d9169e08e508d308404c
Kernel Panic <davil544@gmail.com>
2024-04-06 21:55:29 -0400

    Removed unnecessary code (Removed extra panel that served no purpose)

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -33,15 +33,6 @@ namespace CPIS_Senior_Project
         protected global::System.Web.UI.WebControls.Label LoggedInItems;
 
         /// <summary>
-        /// formNavbarSearch control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel formNavbarSearch;
-
-        /// <summary>
         /// txtSearch control.
         /// </summary>
         /// <remarks>
