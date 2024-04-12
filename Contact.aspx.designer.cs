d7d40163e9a36e1f1c7e779508feefa08d9c01c7
Joseph Borgella <josephborgella@gmail.com>
2024-04-12 16:46:56 -0400

    slight changes

--- Contact.aspx.designer.cs
+++ Contact.aspx.designer.cs
@@ -96,6 +96,15 @@ namespace CPIS_Senior_Project
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
 
         /// <summary>
+        /// RegularExpressionValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
+
+        /// <summary>
         /// ValidationSummary1 control.
         /// </summary>
         /// <remarks>
